2fb96516ffb80a1f569285dc87ffe66520ea6a61
Igor Tron <root@itron.pro>
2022-05-19 00:17:01 +0300

    clients_folder

--- wg_new_user.bash
+++ wg_new_user.bash
@@ -13,17 +13,17 @@ if [ -d $client_name ]; then
 fi
 
 if [ ! -f server_variables ]; then
-	echo "No server_variables file!"
+	echo "server_variables file does not exists"
 	exit 1
 fi
 
-if [ ! -f last_octet_file ]; then
-	echo "No last_octet_file!"
+if [ ! -f last_octet ]; then
+	echo "last_octet file does not exists"
 	exit 1
 fi
 
 source server_variables
-source last_octet_file
+source last_octet
 
 TG_SEND=$(which telegram-send)
 if [ ! -f "$TG_SEND" ]; then
@@ -48,34 +48,40 @@ if [ ! -f "$WG" ]; then
 	exit 1
 fi
 
-mkdir $client_name
+clients_folder=clients
+
+if [ ! -d $clients_folder ]; then
+	mkdir $clients_folder
+fi
+
+mkdir $clients_folder/$client_name
 
 preshared=$(wg genpsk)
-printf $preshared >$client_name/preshared
+printf $preshared >$clients_folder/$client_name/preshared
 
 privatekey=$(wg genkey)
-printf $privatekey >$client_name/privatekey
+printf $privatekey >$clients_folder/$client_name/privatekey
 
-publickey=$(wg pubkey <./$client_name/privatekey)
-printf $publickey >$client_name/publickey
+publickey=$(wg pubkey <./$clients_folder/$client_name/privatekey)
+printf $publickey >$clients_folder/$client_name/publickey
 
 server_public=$(wg show $wg_interface | grep public | awk '{print $3}')
 
 server_port=$(wg show $wg_interface | grep port | awk '{print $3}')
 endpoint="$server_ip:$server_port"
 
-printf "[Interface]\nPrivateKey = $privatekey\nAddress = $wg_network.$last_octet/24\nDNS = 1.1.1.1, 8.8.8.8\n\n[Peer]\nPublicKey = $server_public\nPresharedKey = $preshared\nAllowedIPs = 0.0.0.0/0\nEndpoint = $endpoint\nPersistentKeepalive = 25" >$client_name/wg.conf
-printf "###$client_name config###\n[Peer]\nPublicKey = $publickey\nPresharedKey = $preshared\nAllowedIPs = $wg_network.$last_octet/32\n###End $client_name config###\n" >$client_name/for_server_config.conf
+printf "[Interface]\nPrivateKey = $privatekey\nAddress = $wg_network.$last_octet/24\nDNS = 1.1.1.1, 8.8.8.8\n\n[Peer]\nPublicKey = $server_public\nPresharedKey = $preshared\nAllowedIPs = 0.0.0.0/0\nEndpoint = $endpoint\nPersistentKeepalive = 25" >$clients_folder/$client_name/wg.conf
+printf "###$client_name config###\n[Peer]\nPublicKey = $publickey\nPresharedKey = $preshared\nAllowedIPs = $wg_network.$last_octet/32\n###End $client_name config###\n" >$clients_folder/$client_name/for_server_config.conf
 cat $client_name/for_server_config.conf >>/etc/wireguard/$wg_interface.conf
 
 systemctl restart wg-quick@$wg_interface
 
 ((last_octet++))
-printf "last_octet=$last_octet" >last_octet_file
+printf "last_octet=$last_octet" >last_octet
 
-$QR -t ansiutf8 <$client_name/wg.conf
-$QR -o $client_name/$client_name-qr.png <$client_name/wg.conf
+$QR -t ansiutf8 <$clients_folder/$client_name/wg.conf
+$QR -o $client_name/$client_name-qr.png <$clients_folder/$client_name/wg.conf
 
 printf "$client_name client config for $(hostname) server" | $TG_SEND --stdin
-$TG_SEND --image $client_name/$client_name-qr.png --caption "Client config"
-$TG_SEND --file $client_name/wg.conf
+$TG_SEND --image $clients_folder/$client_name/$client_name-qr.png --caption "Client config"
+$TG_SEND --file $clients_folder/$client_name/wg.conf
